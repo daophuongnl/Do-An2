e77771ffe9c9d56844fa9862199399d2640f1ec6
daophuongnl <127758962+daophuongnl@users.noreply.github.com>
2023-05-30 17:37:02 +0700

    update

--- RevitCommand/0.Command/Command/TestCommand.cs
+++ RevitCommand/0.Command/Command/TestCommand.cs
@@ -5,6 +5,8 @@ using Model.Form;
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using Utility;
+using Autodesk.Revit.DB.Plumbing;
 
 namespace Model.RevitCommand
 {
@@ -13,7 +15,48 @@ namespace Model.RevitCommand
     {
         public override void Execute()
         {
+            var mainPipe = sel.PickElement<Pipe>();
+            var sprinkler = sel.PickElement<FamilyInstance>();
+
+            var sprinklerConnector = sprinkler.MEPModel.ConnectorManager.UnusedConnectors.Cast<Connector>()
+                .First(x => x.CoordinateSystem.BasisZ.IsParallel(XYZ.BasisZ));
+
+            var sprinklerConnectorOrigin = sprinklerConnector.Origin;
+
+            var sprinklerConnectorDirection = sprinklerConnector.CoordinateSystem.BasisZ;
+
+            var mainLocationLine = ((mainPipe.Location as LocationCurve)!.Curve as Line)!;
+
+            var projectPoint = mainLocationLine.GetProjectPoint(sprinklerConnectorOrigin);
+
+            XYZ normal = XYZ.BasisZ;
+            XYZ origin = projectPoint;
+            Plane plane1 = Plane.CreateByNormalAndOrigin(normal, origin);
+
+           PointOnPlane point1 = doc.Application.Create.NewPointOnPlane(plane1,UV.Zero, UV
+
+         
             
+            using (var transaction = new Transaction(doc,"Pipe sprinkler"))
+            {
+                transaction.Start();
+
+                var pipeTypeId = mainPipe.PipeType.Id;
+                var levelId = mainPipe.LookupParameter("Reference Level").AsElementId();
+                var endpoint1 = sprinklerConnectorOrigin + sprinklerConnectorDirection * 100.0.milimeter2Feet();
+                var fisrtPipe = Pipe.Create(doc, pipeTypeId, levelId, sprinklerConnector, endpoint1);
+
+                var diameter = 32.0.milimeter2Feet();
+                fisrtPipe.LookupParameter("Diameter").Set(diameter);
+
+                
+
+                transaction.Commit();
+
+            }
+
+            
+            //var 
         }
     }
 }
